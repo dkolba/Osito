829fcd66aa6d5c761d6eb50638338263f91d622b
duncan <duncankolba@googlemail.com>
2012-04-22 14:38:48 +0200

    Test mit text in Crafty entity

--- public/js/game.js
+++ public/js/game.js
@@ -1,20 +1,68 @@
 window.onload = (function() {
     var WIDTH = 800,
         HEIGHT = 640;
-    // Initialize Crafty
     Crafty.init(WIDTH, HEIGHT);
-    
-    /*
-     * Create an entity with Crafty.e(..) that
-     *  - can be drawn (2D) on a HTML canvas (Canvas)
-     *  - has a background color (Color)
-     *  - can be moved with WASD or arrow keys (Fourway)
-     */
-    var pl = Crafty.e("2D, Canvas, Color, Fourway")
-                .attr({x: 160, y: 96, w: 32, h: 32}) // for Component 2D
-                .color("#eeeeee") // for Component Color
-                .fourway(10); // for Component Fourway
 
-    // log the created entity to the JS console
-    console.log(pl);
+    /*
+     * Create Sprites of size 32 from the PNG file.
+     * There is only one Sprite in the file. It is mostly transparent
+     * so the background color will shine through it.
+     */
+    Crafty.sprite(32, "../../img/crate.png", { crate: [0, 0]});
+
+    /**
+     * This is a simple component, a Box, which again gets capabilities from these
+     * other components:
+     *  - 2D, Canvas: can be drawn on a Canvas
+     *  - Color: has a Background Color
+     *  - Fourway: can be moved with WASD and arrow keys
+     *  - Mouse: reacts on mouse events, like click
+     *  - Tween: simple animation, used here for fading out after mouse click
+     *  - crate: That's the Sprite defined above, not really a JS component
+     */
+    Crafty.c("Box", {
+        init: function() {
+            this.addComponent("2D, Canvas, Color, Fourway, Mouse, Tween, crate, Text");
+
+            this.w = 32;    // width
+            this.h = 32;    // height
+            this.fourway(10);   // initalize 4-way movement
+            this.text("Wer das liest ist doof");
+
+            /*
+             * An 'enterframe' event is created by Crafty for every frame that is
+             * created. Components should update their status in the phase.
+             * Here we check if the alpha gradient is < 0.1, in which case the
+             * entity gets destroyed.
+             * The alpha value gets changed by the Tween component.
+             */
+            this.bind("EnterFrame", function(e) {
+                if (this._alpha < 0.1) {
+                    this.destroy();
+                }
+            });
+            /*
+             * This defines the handler method for mouse clicks.
+             * Here we tell the entity to gradually change its alpha gradient to 0.0.
+             * This is done with the Tween component and it takes 50 frames from 1.0 to 0.0.
+             */
+            this.bind("Click", function(e) {
+                console.log(arguments);
+                this.tween({alpha: 0.0}, 50);
+            });
+        },
+        /**
+         * Convenience method which sets the box position and color
+         */
+        makeBox: function(x, y, color) {
+            this.attr({x: x, y: y}).color(color);
+        }
+    });
+
+    // create 5 boxes of different colors and place them on the canvas
+    Crafty.e("Box").makeBox(160, 96, "#F00");
+    Crafty.e("Box").makeBox(240, 96, "#0F0");
+    Crafty.e("Box").makeBox(320, 96, "#FF0");
+    Crafty.e("Box").makeBox(400, 96, "#F0F");
+    Crafty.e("Box").makeBox(480, 96, "#0FF");
 });
