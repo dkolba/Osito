1a010ed241129861fb26c69c79b351ea68cf64dc
Duncan Kolba <duncankolba@gmail.com>
2013-02-23 23:16:59 +0100

    Load SVG image onto canvas

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,9 @@
-  var canvas = document.getElementById("oCanvas");
-  var ctx = canvas.getContext("2d");
+var ctx= document.getElementById("oCanvas").getContext("2d")
+  , img = new Image();
+
+img.src = '/img/01.svg';
+img.onload = function() {
+  ctx.drawImage(img, 10, 10, 100, 100);
+  ctx.drawImage(img, 160, 10, 100, 100);
+  ctx.drawImage(img, 310, 10, 100, 100);
+};
